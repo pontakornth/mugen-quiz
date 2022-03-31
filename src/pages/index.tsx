7d02fee51c42b4110372af9e778fb05c3c933587
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-03-31 12:13:56 +0700

    Add navigating the questions page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import { route } from "preact-router"
+
 export function Index() {
+	const handleClick = () => {
+		route("/questions/1")
+	}
 	return (
 		<>
 			<h1>Mugen Quiz</h1>
 			<p>Determining your language like a boss.</p>
-			<button>Start the quiz</button>
+			<button onClick={handleClick}>Start the quiz</button>
 		</>
 	)
 }
